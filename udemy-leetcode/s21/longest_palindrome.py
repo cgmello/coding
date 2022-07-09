e586b53b82a5d39455d25f414a40c2c9fdfa3bf3
Claudio Mello <cgmello@gmail.com>
2022-07-09 00:06:23 -0300

    longest palindrome: making more verbose

--- udemy-leetcode/s21/longest_palindrome.py
+++ udemy-leetcode/s21/longest_palindrome.py
@@ -5,19 +5,26 @@ class Solution:
         if n < 2:
             return s
 
+        # create a matrix NxN all marked as false
         palindrome = [[False for i in range(n)] for i in range(n)]
+        print(palindrome)
 
+        # single characters are palindromes
         for i in range(n):
             palindrome[i][i] = True
 
+        # answer is the limits of the substring
         left = 0
         right = 0
 
         for j in range(1, n):
             for i in range(0, j):
+                # j-2<=2 has no inner substring (eg "ab")
                 inner = palindrome[i + 1][j - 1] or j - i <= 2
+                # first char = last char and inner is palindrome ?
                 if s[i] == s[j] and inner:
                     palindrome[i][j] = True
+                    # update biggest substring that is palindrome
                     if j - i > right - left:
                         left = i
                         right = j
